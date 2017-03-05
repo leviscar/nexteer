186f7978a64096326da636521e53f8207e71d0a3
leviscar <liujunpeng1994@foxmail.com>
2017-03-05 12:30:09 +0800

    安全运行主要模块完成

--- src/main/resources/static/js/safeset.js
+++ src/main/resources/static/js/safeset.js
@@ -14,9 +14,19 @@ function safety_date(year, month, day, is_safe, safety_date, log) {
     var Umonth=Udate.getMonth()+1;
     var Uday=Udate.getDate();
     $("#time").html("公元"+Uyear+"年"+Umonth+"月"+Uday+"日");
-    var jsonString = new safety_date("2016", "03", "23");
+    var UsafeMonth=Umonth.toString();
+    var UsafeDay=Uday.toString();
+    if(Umonth<10&&Umonth>0){
+        USafeMonth="0"+Umonth.toString();
+    }
+    if(UsafeDay<10&&UsafeDay>0){
+        UsafeDay="0"+UsafeDay.toString();
+    }
 
-$("#addDay").bind("click", function () {
+    var jsonString = new safety_date(Uyear.toString(),UsafeMonth , UsafeDay);
+$("#safeDay").html("shij");
+    //更新当前安全天数
+$("#addDay").bind("click",function () {
     $.ajax({
         type: "POST",
         url: "http://localhost:8080/safetyDate/addDate",
@@ -25,15 +35,12 @@ $("#addDay").bind("click", function () {
         dataType: "json",
         success: function (data) {
             $(".loading").html(JSON.stringify(data));
-            $("#submit-day").html("运行安全");
+            $("#submit-day").html("上传成功");
         },
         failure: function (errMsg) {
             console.log(errMsg);
         }
     });
-});
-
-$("#getDay").bind("click", function () {
     $.ajax({
         type: "POST",
         url: "http://localhost:8080/safetyDate/getDates",
@@ -42,72 +49,102 @@ $("#getDay").bind("click", function () {
         dataType: "json",
         success: function (data) {
             $(".loading").html(JSON.stringify(data));
-            $("#submit-day").html("运行安全");
+            $("#safeDay").html(data.safe_dates);
+            $("#res").html("获取指定日期正常");
         },
         failure: function (errMsg) {
             console.log(errMsg);
         }
     });
 });
-// $("#getDay").bind("click", function () {
-//     $.ajax({
-//         type: "POST",
-//         url: "http://localhost:8080/safetyDate/getDates",
-//         data: JSON.stringify(jsonString),
-//         contentType: "application/json; charset=utf-8",
-//         dataType: "json",
-//         success: function (data) {
-//             $("#res").html(JSON.stringify(data));
-//         },
-//         failure: function (errMsg) {
-//             console.log(errMsg);
-//         }
-//     });
-// });
-// $(document).ready(function ()
-// {   var Udate=new Date();
-//     var Uyear=Udate.getFullYear();
-//     var Umonth=Udate.getMonth()+1;
-//     var Uday=Udate.getDate();
-//     $("#time").html("公元"+Uyear+"年"+Umonth+"月"+Uday+"日");
-//     $("#addDay").click(change());
-//     function change() {
-//         $("#submit-day").html("ahahaha");
-// // $("#submit-day").html("嗯哼"+$("#myYear").val());
-//     }
-//     $("#getDay").click(function () {
-//
-//         var data={
-//         "year":"2007",
-//         "month":"02",
-//         "day":"01"
-//         };
-//         $("#safeDay").html($.toJSON({"year":"2007","month":"02","day":"01"}));
-//         // // $(".text").attr('disable','true');
-//         // // $('.loading').show();
-//
-//         // $.ajax({
-//         //     url:"http://localhost:8080/safetyDate/getDates",
-//         //     type:"POST",
-//         //     data:carr,
-//         //     success:function (data) {
-//         //
-//         //     $("#submit-day").html("接收nimei");
-//         //     $(".loading").html(JSON.stringify(data));
-//         //
-//         //     }
-//         // });
-//         $.ajax({
-//         url:"http://localhost:8080/safetyDate/getDates",
-//         type:"POST",
-//         data:"year=2007&month=02&day=01",
-//         success:function (data) {
-//
-//         $("#submit-day").html("接收成功");
-//         $(".loading").html(JSON.stringify(data));
-//
-//         }
-//         })
-//     })
-//
-// });
+
+
+//获得指定日期安全天数
+$("#getDay").bind("click", function () {
+    var getDayYear = $("#myYear").val();
+    var getDayMonth = $("#myMonth").val();
+    var getDayDay = $("#myDay").val();
+
+    var getDayJson = new safety_date(getDayYear.toString(), getDayMonth.toString(), getDayDay.toString());
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/safetyDate/getDates",
+        data: JSON.stringify(getDayJson),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $(".loading").html(JSON.stringify(data));
+            $("#safeSearch").html(data.safe_dates);
+            $("#res").html("获取指定日期正常");
+        },
+        failure: function (errMsg) {
+            console.log(errMsg);
+        }
+    });
+});
+
+//单击重置安全日期
+$("#startDate").bind("click",function () {
+    function startDay(year, month, day, safe_dates, log) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.safe_dates = safe_dates;
+        this.log = log;
+    }
+    var startDateYear=$("#startYear").val();
+    var startDateMonth=$("#startMonth").val();
+    var startDateDay=$("#startDay").val();
+    var startCount=$("#startSafeDay").val();
+    var startLog="";
+
+    var getStartJson=new startDay(startDateYear.toString(),startDateMonth.toString(),startDateDay.toString(),startCount.toString(),startLog.toString());
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/safetyDate/reset",
+        data: JSON.stringify(getStartJson),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $(".loading").html(JSON.stringify(data));
+            $("#safeDay").html(data.safe_dates);
+            $("#submit-day").html("普通重置运行安全");
+        },
+        failure: function (errMsg) {
+            console.log(errMsg);
+        }
+    });
+});
+
+
+//发生事故重置
+$("#resetDate").bind("click",function () {
+    function resetDate(year, month, day, safety_date, log) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.safe_dates = safety_date;
+        this.log = log;
+    }
+    var resetYear=$("#resetYear").val().toString();
+    var resetMonth=$("#resetMonth").val().toString();
+    var resetDay=$("#resetDay").val().toString();
+    var resetMessage=$("#resetMessage").val().toString();
+
+    var getResetJson=new resetDate(resetYear,resetMonth,resetDay,0,resetMessage);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/safetyDate/reset",
+        data: JSON.stringify(getResetJson),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $(".loading").html(JSON.stringify(data));
+            $("#safeDay").html(data.safe_dates);
+            $("#submit-day").html("发生事故重置运行安全");
+        },
+        failure: function (errMsg) {
+            console.log(errMsg);
+        }
+    });
+});
