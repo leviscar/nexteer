0fc2297305801d57836d81a6b7efe02272b2d60d
leviscar <liujunpeng1994@foxmail.com>
2017-03-05 21:48:38 +0800

    修改了日历页面，将数据库中安全日期标绿，不安全日期标红

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -4,6 +4,13 @@
 
 
 var circleDateTime=[];
+var safeCalendarTime=[];
+$.get("http://localhost:8080/safetyDate/getAllDates", function (data) {
+    $.each(data, function (i, model) {
+        safeCalendarTime[i]=new Date(model.year,model.month-1,model.day).getTime();
+    })
+});
+
 $.get("http://localhost:8080/safetyDate/getUnsafeDates", function (data) {
     $.each(data, function (i, model) {
         circleDateTime[i]=new Date(model.year,model.month-1,model.day).getTime();
@@ -13,8 +20,15 @@ $.get("http://localhost:8080/safetyDate/getUnsafeDates", function (data) {
 $(function() {
     $('#calendar').calendar({
         customDayRenderer: function(element, date) {
-            for(var i in circleDateTime){
-                if(date.getTime()==circleDateTime[i]){
+            for(var i in safeCalendarTime){
+                if(date.getTime()==safeCalendarTime[i]){
+                    $(element).css('background-color', 'green');
+                    $(element).css('color', 'white');
+                    $(element).css('border-radius', '15px');
+                }
+            }
+            for(var j in circleDateTime){
+                if(date.getTime()==circleDateTime[j]){
                     $(element).css('background-color', 'red');
                     $(element).css('color', 'white');
                     $(element).css('border-radius', '15px');
