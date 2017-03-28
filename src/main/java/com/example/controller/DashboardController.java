80bcd6234b33c64b69a35205a225d1ad9609e00b
desktop\mrpan <mrpancheng@126.com>
2017-03-28 21:18:44 +0800

    增加主面板hce信息获取

--- src/main/java/com/example/controller/DashboardController.java
+++ src/main/java/com/example/controller/DashboardController.java
@@ -2,13 +2,10 @@ package com.example.controller;
 
 import com.example.enumtype.Cell;
 import com.example.enumtype.ShiftType;
-import com.example.model.DashboardOutput;
-import com.example.model.Ishaft1Product;
-import com.example.model.SafetyDate;
-import com.example.model.WorkShift;
-import com.example.repository.Ishaft1ProductRepo;
-import com.example.repository.WorkShiftRepo;
+import com.example.model.*;
+import com.example.repository.*;
 import com.example.util.Function;
+import com.example.util.ModelOutput;
 import com.example.util.OutputTool;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -22,11 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import static com.example.enumtype.Cell.CEPS;
 import static com.example.enumtype.Cell.ISHAFT1;
 
 /**
@@ -37,15 +34,22 @@ import static com.example.enumtype.Cell.ISHAFT1;
 public class DashboardController {
     private WorkShiftRepo workShiftRepo;
     private Ishaft1ProductRepo ishaft1ProductRepo;
+    private ProductModelRepo productModelRepo;
+    private RestEventWithWorkShiftRepo restEventWithWorkShiftRepo;
+    private RestEventRepo restEventRepo;
 
     @Autowired
-    public DashboardController(WorkShiftRepo workShiftRepo, Ishaft1ProductRepo ishaft1ProductRepo) {
+    public DashboardController(WorkShiftRepo workShiftRepo, Ishaft1ProductRepo ishaft1ProductRepo, ProductModelRepo productModelRepo, RestEventWithWorkShiftRepo restEventWithWorkShiftRepo, RestEventRepo restEventRepo) {
         this.workShiftRepo = workShiftRepo;
         this.ishaft1ProductRepo = ishaft1ProductRepo;
+        this.productModelRepo = productModelRepo;
+        this.restEventWithWorkShiftRepo = restEventWithWorkShiftRepo;
+        this.restEventRepo = restEventRepo;
     }
 
     /**
      * 主面板产量信息展示
+     *
      * @param json
      * @return
      * @throws ParseException
@@ -79,6 +83,7 @@ public class DashboardController {
 
     /**
      * 获得主面板的oee
+     *
      * @param json
      * @return
      */
@@ -127,6 +132,100 @@ public class DashboardController {
                 break;
         }
         return object.toString();
+    }
+
+    /**
+     * 获取hce
+     *
+     * @param json
+     * @return
+     * @throws ParseException
+     */
+    @RequestMapping(value = "hce", method = RequestMethod.POST)
+    public String getHce(@RequestBody String json) throws ParseException {
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(json);
+        JsonObject jsonObject = element.getAsJsonObject();
+        String cellName = new Gson().fromJson(jsonObject.get("cell_name"), String.class);
+        Date curDate = new Gson().fromJson(jsonObject.get("curr_time"), Date.class);
+        // 获得最新的班次信息
+        WorkShift workShift = workShiftRepo.getLatestWorkShift().get(0);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String curTime = sdf.format(curDate);
+        ShiftType shiftType = OutputTool.getShiftType(workShift, sdf.parse(curTime));
+
+        JsonObject object = new JsonObject();
+        if (shiftType == null) {
+            object.addProperty("status", false);
+            object.addProperty("log", "当前时刻不在任何班次中");
+            return object.toString();
+        }
+        // 设置日期
+        List<Date> dateList = OutputTool.changeShiftDate(curDate, workShift, shiftType);
+        Date startDate = dateList.get(0);
+        Map<Date, Boolean> addDateRes = Function.addOneDay(startDate, curDate);
+        curDate = (Date) addDateRes.keySet().toArray()[0];
+        Cell cell = Cell.valueOf(cellName);
+        double hce = 0;
+        switch (cell) {
+            case ISHAFT1:
+                int workerNum = 0;
+                int overtimeWorkerNum = 0;
+                switch (shiftType) {
+                    case MORNING_SHIFT:
+                        workerNum = workShift.getMorning_worker_num();
+                        overtimeWorkerNum = workShift.getMorning_overtime_worker_num();
+                        break;
+                    case MIDDLE_SHIFT:
+                        workerNum = workShift.getMiddle_worker_num();
+                        overtimeWorkerNum = workShift.getMiddle_overtime_worker_num();
+                        break;
+                    case NIGHT_SHIFT:
+                        workerNum = workShift.getNight_worker_num();
+                        overtimeWorkerNum = workShift.getNight_overtime_worker_num();
+                        break;
+                }
+                int standardMinutes = 8 * 60;
+                List<Ishaft1Product> products = ishaft1ProductRepo.getByPeriod(startDate, curDate);
+                // 计算所有型号乘对应的std的值
+                Map<String, Integer> modelOutputMap = ModelOutput.getEachModelOutput(products);
+                float stdMultiplyOutput = 0;
+                for (Map.Entry<String, Integer> entry : modelOutputMap.entrySet()) {
+                    String modelId = entry.getKey();
+                    ProductModel model = productModelRepo.getStdByModelId(modelId);
+                    stdMultiplyOutput += model.getStd() * entry.getValue();
+                }
+                long restSeconds = getRestSeconds(workShift.getId(), shiftType, sdf.parse(curTime));
+                long totalSeconds = (curDate.getTime() - startDate.getTime()) / 1000;
+                if (totalSeconds / 60 > standardMinutes) {
+                    // 当前为止的加班时间
+                    int overMinutes = (int) (totalSeconds / 60 - standardMinutes);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(startDate);
+                    calendar.add(Calendar.MINUTE, standardMinutes);
+                    Date endTime = calendar.getTime();
+                    // 正常工作的总休息时间
+                    int normalRestMinute = (int) (getHourlyRestSeconds(workShift.getId(), shiftType, startDate, endTime) / 60);
+                    // 加班时间内的休息时间
+                    int overRestMinutes = (int) (restSeconds / 60 - normalRestMinute);
+                    hce = 100 * stdMultiplyOutput * 60 / ((standardMinutes - normalRestMinute) * workerNum + (overMinutes - overRestMinutes) * overtimeWorkerNum);
+                } else {
+                    hce = 100 * stdMultiplyOutput * 60 * 60 / ((totalSeconds - restSeconds) * workerNum);
+                }
+                break;
+            case ISHAFT2:
+                break;
+            case ISHAFT3:
+                break;
+            case ISHAFT4:
+                break;
+            case BEPS:
+                break;
+            case CEPS:
+                break;
+        }
+        object.addProperty("hce", hce);
+        return new Gson().toJson(object);
     }
 
     /**
@@ -198,4 +297,58 @@ public class DashboardController {
         return new Gson().toJson(dashboardOutput);
     }
 
+    private long getHourlyRestSeconds(int workShiftId, ShiftType shiftType, Date startTime, Date endTime) throws ParseException {
+        List<RestEventWithWorkShift> restEventWithWorkShiftList = restEventWithWorkShiftRepo.getByWorkShiftId(workShiftId);
+        if (restEventWithWorkShiftList.isEmpty()) {
+            return 0;
+        } else {
+            long restSeconds = 0;
+            for (RestEventWithWorkShift restEventWithWorkShift : restEventWithWorkShiftList) {
+                RestEvent event = restEventRepo.getById(restEventWithWorkShift.getId());
+                // 若休息时间在班次内
+                if (shiftType.toString().equals(event.getShift_type())) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                    Date eventStartTime = sdf.parse(event.getEvent_start_time());
+                    Date eventEndTime = sdf.parse(event.getEvent_end_time());
+                    if (endTime.before(eventStartTime) || startTime.after(eventEndTime)) {
+                        restSeconds += 0;
+                    } else if (startTime.after(eventStartTime) && endTime.before(eventEndTime)) {
+                        restSeconds += (endTime.getTime() - startTime.getTime()) / 1000;
+                    } else if (startTime.before(eventStartTime) && endTime.before(eventEndTime)) {
+                        restSeconds += (endTime.getTime() - eventStartTime.getTime()) / 1000;
+                    } else if (startTime.after(eventStartTime) && endTime.after(eventEndTime)) {
+                        restSeconds += (eventEndTime.getTime() - startTime.getTime()) / 1000;
+                    } else if (startTime.before(eventStartTime) && endTime.after(eventEndTime)) {
+                        restSeconds += (eventEndTime.getTime() - eventStartTime.getTime()) / 1000;
+                    }
+                }
+            }
+            return restSeconds;
+        }
+    }
+
+    private long getRestSeconds(int workShiftId, ShiftType curShiftType, Date curTime) throws ParseException {
+        List<RestEventWithWorkShift> restEventWithWorkShiftList = restEventWithWorkShiftRepo.getByWorkShiftId(workShiftId);
+        if (restEventWithWorkShiftList.isEmpty()) {
+            return 0;
+        } else {
+            long restSeconds = 0;
+            for (RestEventWithWorkShift restEventWithWorkShift : restEventWithWorkShiftList) {
+                RestEvent event = restEventRepo.getById(restEventWithWorkShift.getId());
+                // 若休息时间在班次内
+                if (curShiftType.toString().equals(event.getShift_type())) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                    Date startTime = sdf.parse(event.getEvent_start_time());
+                    Date endTime = sdf.parse(event.getEvent_end_time());
+                    // 若当前时刻在休息时间之中
+                    if (curTime.before(endTime) && curTime.after(startTime)) {
+                        restSeconds += curTime.getTime() - startTime.getTime();
+                    } else if (curTime.after(endTime)) {
+                        restSeconds += endTime.getTime() - startTime.getTime();
+                    }
+                }
+            }
+            return restSeconds / 1000;
+        }
+    }
 }
