99c1622dff6936a0dae997e3b6767a0756e72762
desktop\mrpan <mrpancheng@126.com>
2017-03-28 14:49:01 +0800

    增加主面板oee信息获取

--- src/main/java/com/example/controller/DashboardController.java
+++ src/main/java/com/example/controller/DashboardController.java
@@ -4,6 +4,7 @@ import com.example.enumtype.Cell;
 import com.example.enumtype.ShiftType;
 import com.example.model.DashboardOutput;
 import com.example.model.Ishaft1Product;
+import com.example.model.SafetyDate;
 import com.example.model.WorkShift;
 import com.example.repository.Ishaft1ProductRepo;
 import com.example.repository.WorkShiftRepo;
@@ -25,24 +26,31 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import static com.example.enumtype.Cell.CEPS;
 import static com.example.enumtype.Cell.ISHAFT1;
 
 /**
  * Created by mrpan on 2017/3/28.
  */
 @RestController
-@RequestMapping(value = "dashboard")
-public class DashboardOutputController {
+@RequestMapping(value = "/dashboard")
+public class DashboardController {
     private WorkShiftRepo workShiftRepo;
     private Ishaft1ProductRepo ishaft1ProductRepo;
 
     @Autowired
-    public DashboardOutputController(WorkShiftRepo workShiftRepo, Ishaft1ProductRepo ishaft1ProductRepo) {
+    public DashboardController(WorkShiftRepo workShiftRepo, Ishaft1ProductRepo ishaft1ProductRepo) {
         this.workShiftRepo = workShiftRepo;
         this.ishaft1ProductRepo = ishaft1ProductRepo;
     }
 
-    @RequestMapping(value = "output", method = RequestMethod.POST)
+    /**
+     * 主面板产量信息展示
+     * @param json
+     * @return
+     * @throws ParseException
+     */
+    @RequestMapping(value = "/output", method = RequestMethod.POST)
     public String getAllOutput(@RequestBody String json) throws ParseException {
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(json);
@@ -67,6 +75,58 @@ public class DashboardOutputController {
                 break;
         }
         return res;
+    }
+
+    /**
+     * 获得主面板的oee
+     * @param json
+     * @return
+     */
+    @RequestMapping(value = "/oee", method = RequestMethod.POST)
+    public String getOee(@RequestBody String json) throws ParseException {
+        int topN = 30;
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(json);
+        JsonObject jsonObject = element.getAsJsonObject();
+        Date curDate = new Gson().fromJson(jsonObject.get("curr_time"), Date.class);
+        String cellName = new Gson().fromJson(jsonObject.get("cell_name"), String.class);
+
+        // 获得最新的班次信息
+        WorkShift workShift = workShiftRepo.getLatestWorkShift().get(0);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String curTime = sdf.format(curDate);
+        ShiftType shiftType = OutputTool.getShiftType(workShift, sdf.parse(curTime));
+
+        JsonObject object = new JsonObject();
+        if (shiftType == null) {
+            object.addProperty("status", false);
+            object.addProperty("log", "当前时刻不在任何班次中");
+            return object.toString();
+        }
+        // 设置日期
+        List<Date> dateList = OutputTool.changeShiftDate(curDate, workShift, shiftType);
+        Date startDate = dateList.get(0);
+        Map<Date, Boolean> addDateRes = Function.addOneDay(startDate, curDate);
+        curDate = (Date) addDateRes.keySet().toArray()[0];
+        Cell cell = Cell.valueOf(cellName);
+        switch (cell) {
+            case ISHAFT1:
+                List<Date> topNProducts = ishaft1ProductRepo.getCurBeats(startDate, curDate, topN);
+                int curBeats = OutputTool.calcCurBeats(topNProducts, topN);
+                object.addProperty("oee", curBeats);
+                break;
+            case ISHAFT2:
+                break;
+            case ISHAFT3:
+                break;
+            case ISHAFT4:
+                break;
+            case BEPS:
+                break;
+            case CEPS:
+                break;
+        }
+        return object.toString();
     }
 
     /**
