6d8c989222e154d843e8dbfbddbe90378f5b8db6
desktop\mrpan <mrpancheng@126.com>
2017-04-18 15:03:00 +0800

    Add function of delete and update in product model fix #8

--- src/main/java/com/example/controller/ProductModelController.java
+++ src/main/java/com/example/controller/ProductModelController.java
@@ -4,17 +4,16 @@ import com.example.model.ProductModel;
 import com.example.repository.ProductModelRepo;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
  * Created by mrpan on 2017/3/26.
  */
 @RestController
+@RequestMapping(value = "/product-model")
 public class ProductModelController {
     private ProductModelRepo repo;
 
@@ -24,25 +23,59 @@ public class ProductModelController {
     }
 
     /**
-     * 添加型号信息
+     * Add product model record into database
      *
      * @param json
      * @return
      */
-    @RequestMapping(value = "/product-model", method = RequestMethod.POST)
-    public String add(@RequestBody String json) {
+    @RequestMapping(method = RequestMethod.POST)
+    public ProductModel add(@RequestBody String json) {
         Gson gson = new Gson();
         ProductModel model = gson.fromJson(json, ProductModel.class);
-        return repo.addModel(model);
+        return repo.add(model);
     }
 
     /**
-     * 获得所有型号的信息
+     * Get all records
      *
      * @return
      */
-    @RequestMapping(value = "product-model", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<ProductModel> get() {
         return repo.getAllModel();
     }
+
+    /**
+     * Get the records by cell name
+     *
+     * @param cellName
+     * @return
+     */
+    @RequestMapping(value = "/{cell}", method = RequestMethod.GET)
+    public List<ProductModel> getByCell(@PathVariable(value = "cell") String cellName) {
+        return repo.getModelByCellName(cellName);
+    }
+
+    /**
+     * Update record
+     * @param model
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.PATCH)
+    public ProductModel update(@RequestBody ProductModel model){
+        return repo.update(model);
+    }
+
+    /**
+     * Delete record based on model id
+     * @param model
+     * @param response
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void delete(@RequestBody ProductModel model, HttpServletResponse response){
+        repo.deleteByModelId(model.getModelId());
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
+
 }
