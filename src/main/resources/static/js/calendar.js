27b758b13c93957b3f4409a44d8bd8512421bba3
leviscar <liujunpeng1994@foxmail.com>
2017-04-23 02:55:15 +0800

    增加了安全运行天数以及质量无抱怨天数二级页面显示事故时间

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -5,6 +5,7 @@
 
 var circleDateTime=[];
 var safeCalendarTime=[];
+var eventData = [];
 $.get("http://localhost:8080/nexteer/safety-date", function (data) {
     $.each(data, function (i, model) {
         safeCalendarTime[i]=new Date(model.year,model.month-1,model.day).getTime();
@@ -14,11 +15,42 @@ $.get("http://localhost:8080/nexteer/safety-date", function (data) {
 $.get("http://localhost:8080/nexteer/safety-date/unsafe", function (data) {
     $.each(data, function (i, model) {
         circleDateTime[i]=new Date(model.year,model.month-1,model.day).getTime();
-    })
+        eventData[i]={id:i,color:'red',name:model.log,startDate: new Date(model.year,model.month-1,model.day),endDate: new Date(model.year,model.month-1,model.day)};
+    });
+    console.log(eventData);
 });
 
 $(function() {
     $('#calendar').calendar({
+        mouseOnDay: function(e) {
+            if(e.events.length > 0) {
+                var content = '';
+
+                for(var i in e.events) {
+                    content += '<div class="event-tooltip-content">'
+                        + '<div class="event-name" style="color:' + e.events[i].color + '">' + e.events[i].name + '</div>'
+                        + '</div>';
+                }
+
+                $(e.element).popover({
+                    trigger: 'manual',
+                    container: 'body',
+                    html:true,
+                    content: content
+                });
+
+                $(e.element).popover('show');
+            }
+        },
+        mouseOutDay: function(e) {
+            if(e.events.length > 0) {
+                $(e.element).popover('hide');
+            }
+        },
+        dayContextMenu: function(e) {
+            $(e.element).popover('hide');
+        },
+        dataSource:eventData,
         customDayRenderer: function(element, date) {
             for(var i in safeCalendarTime){
                 if(date.getTime()==safeCalendarTime[i]){
--- src/main/resources/static/js/noComSec.js
+++ src/main/resources/static/js/noComSec.js
@@ -4,6 +4,7 @@
 
 var circleNComDateTime=[];
 var NComCalendarTime=[];
+var eventData =[];
 $.get("http://localhost:8080/nexteer/quality-complain", function (data) {
     $.each(data, function (i, model) {
         var modelTime= model.addDate.split("-");
@@ -16,16 +17,46 @@ $.get("http://localhost:8080/nexteer/quality-complain/no-complain", function (da
     $.each(data, function (i, model) {
         var modelTime= model.addDate.split("-");
         circleNComDateTime[i]=new Date(Number(modelTime[0]),Number(modelTime[1]),Number(modelTime[2])).getTime();
+        eventData[i]={id:i,color:'red',name:model.log,startDate: new Date(Number(modelTime[0]),Number(modelTime[1]),Number(modelTime[2])),endDate: new Date(Number(modelTime[0]),Number(modelTime[1]),Number(modelTime[2]))};
     });
     console.log(circleNComDateTime);
 });
 
 $(function() {
     $('#ncomCalendar').calendar({
+        mouseOnDay: function(e) {
+            if(e.events.length > 0) {
+                var content = '';
+
+                for(var i in e.events) {
+                    content += '<div class="event-tooltip-content">'
+                        + '<div class="event-name" style="color:' + e.events[i].color + '">' + e.events[i].name + '</div>'
+                        + '</div>';
+                }
+
+                $(e.element).popover({
+                    trigger: 'manual',
+                    container: 'body',
+                    html:true,
+                    content: content
+                });
+
+                $(e.element).popover('show');
+            }
+        },
+        mouseOutDay: function(e) {
+            if(e.events.length > 0) {
+                $(e.element).popover('hide');
+            }
+        },
+        dayContextMenu: function(e) {
+            $(e.element).popover('hide');
+        },
+        dataSource:eventData,
         customDayRenderer: function(element, date) {
             for(var i in NComCalendarTime){
                 if(date.getTime()==NComCalendarTime[i]){
-                    $(element).css('background-color', 'green');
+                    $(element).css('background-color', 'orange');
                     $(element).css('color', 'white');
                     $(element).css('border-radius', '15px');
                 }
