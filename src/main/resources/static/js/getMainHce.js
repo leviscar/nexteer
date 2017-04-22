e3b6ac48171c5a20d15f929dd2d2305cf680e2fa
leviscar <liujunpeng1994@foxmail.com>
2017-04-22 09:47:51 +0800

    4-22修改

--- src/main/resources/static/js/getMainHce.js
+++ src/main/resources/static/js/getMainHce.js
@@ -1,6 +1,8 @@
 /**
  * Created by Administrator on 2017/3/30.
  */
+
+var HCEValue = [];
 function judgeTime(time) {
     var timeStr="";
     if (time<10){
@@ -15,33 +17,23 @@ function mainHceInput(curr_time,cell_name) {
     this.curr_time=curr_time;
     this.cell_name=cell_name;
 }
-function getMainHce() {
+function getMainIshaft1Hce() {
     var currTime = judgeTime(Uyear)+"-"+judgeTime(Umonth)+"-"+judgeTime(Uday)+" "+judgeTime(Udate.getHours())+":"+judgeTime(Udate.getMinutes())+":"+judgeTime(Udate.getSeconds());
-    // var currTime ="2017-03-23 15:00:00";
+
     var cellName = "ISHAFT1";
     var mainHceJson = new mainHceInput(currTime,cellName);
     console.log(mainHceJson);
     console.log("getMainHce开始");
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8080/nexteer/dashboard/hce",
-        data: JSON.stringify(mainHceJson),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data) {
-            if(data.system_status != false){
-                console.log(JSON.stringify(data));
-                console.log("获取"+cellName+"HCE成功");
-                $("#showMainIshaft1Hce").html(data.hce.toFixed(1)+"%");
-            }
+    $.get("http://localhost:8080/nexteer/dashboard/hce/ISHAFT1?time="+curTime,function (data) {
+        if(data.system_status != false){
+            console.log(JSON.stringify(data));
 
-        },
-        failure: function (errMsg) {
-            console.log(errMsg);
+            $("#showMainIshaft1Hce").html(data.hce);
+            console.log("获取hce操作成功");
         }
     });
 
 }
 
-getMainHce();
-setInterval(getMainHce(),1000*2);
+getMainIshaft1Hce();
+setInterval(getMainIshaft1Hce(),1000*2);
