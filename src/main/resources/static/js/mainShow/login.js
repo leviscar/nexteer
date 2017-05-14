7d8b471a1bdda0860d27d303bb369649b73f607d
leviscar <liujunpeng1994@foxmail.com>
2017-05-14 21:18:25 +0800

    设置页面url修复

--- src/main/resources/static/js/mainShow/login.js
+++ src/main/resources/static/js/mainShow/login.js
@@ -17,7 +17,7 @@ $("#loginSub").bind("click",function () {
         success: function (data) {
             console.log(JSON.stringify(data));
             if(data.valid==true){
-                window.location.assign("../../html/basicConf.html");
+                window.location.assign("http://10.1.0.40:8080/nexteer/html/basicConf.html");
             }
             else {
                 $("#loginStu").html("用户名或者密码错误")
